5c821a9acc4a54c7535150688319f04e447d5ba2
Derek <drod2169@gmail.com>
2013-08-05 09:51:44 -0400

    Configure libhwui

--- jf-common.mk
+++ jf-common.mk
@@ -171,5 +171,17 @@ $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
-# call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+# Configure libhwui
+# based on flo
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.hwui.texture_cache_size=48 \
+        ro.hwui.layer_cache_size=32 \
+        ro.hwui.r_buffer_cache_size=4 \
+        ro.hwui.path_cache_size=24 \
+        ro.hwui.gradient_cache_size=1 \
+        ro.hwui.drop_shadow_cache_size=5 \
+        ro.hwui.texture_cache_flushrate=0.5 \
+        ro.hwui.text_small_cache_width=1024 \
+        ro.hwui.text_small_cache_height=1024 \
+        ro.hwui.text_large_cache_width=2048 \
+        ro.hwui.text_large_cache_height=1024
